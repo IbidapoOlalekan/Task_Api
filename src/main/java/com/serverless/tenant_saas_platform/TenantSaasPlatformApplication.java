899e20a6e0d70a0b331fbeef717bfb5a11ad93f5
AbdulAzeez Ibidapo <abdulazeezibidapo@MacBook-Pro-2.broadband>
2025-02-23 13:48:09 +0000

    Initial commit of Task API

---
+++ src/main/java/com/serverless/tenant_saas_platform/TenantSaasPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.serverless.tenant_saas_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenantSaasPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenantSaasPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serverless/tenant_saas_platform/TenantSaasPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverless.tenant_saas_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenantSaasPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
